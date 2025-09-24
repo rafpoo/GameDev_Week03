df3dccce1c3f6386ef81c943d837c57ae3468ba6
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-09-24 22:25:52 +0700

    feat: Challenge 1

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     GameObject hero;
+
+    private float currentDistance;
+    private Vector3 offset;
+    private float zoomSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
         hero = GameObject.Find("hero");
+
+        offset = transform.position - hero.transform.position;
+        currentDistance = offset.magnitude;
     }
 
     // Update is called once per frame
@@ -24,6 +31,20 @@ public class CameraScript : MonoBehaviour
         {
             speed = -50;
         }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            currentDistance -= zoomSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            currentDistance += zoomSpeed * Time.deltaTime;
+        }
+
+        offset = offset.normalized * currentDistance;
+
+        transform.position = hero.transform.position + offset;
+
         transform.LookAt(hero.transform);
         transform.RotateAround(hero.transform.position, Vector3.up, speed * Time.deltaTime);
     }
