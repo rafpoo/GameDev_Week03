381eefc763e2fc89fd0f0a6d13f4da981ab96bfb
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-09-24 21:19:20 +0700

    feat: AngleAxis

---
+++ Assets/Scripts/CCTV2Script.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCTV2Script : MonoBehaviour
+{
+    GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("hero");
+        transform.rotation = Quaternion.AngleAxis(45, Vector3.up);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 jarakCctvHero = target.transform.position - transform.position;
+        float angle = Mathf.Atan2(jarakCctvHero.x, jarakCctvHero.z) * Mathf.Rad2Deg;
+
+        Quaternion rotasi = Quaternion.AngleAxis(angle, Vector3.up);
+        transform.rotation = rotasi;
+    }
+}
